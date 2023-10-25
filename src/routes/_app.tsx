9942bdbfc9b79f83a07d954facece81f1ee49ecc
Lucas O. S <ovniroto@gmail.com>
2023-10-25 13:50:02 +0200

    fix: fix background bug with svg, add webp then, close #11

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -8,13 +8,13 @@ export default function App({ Component }: AppProps) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>Confettis!</title>
 				<meta name="description" content="Confettis! 🎉 This is a library to create confettis on your website."></meta>
-				<link href="./favicon.ico?v=1" rel="shortcut icon" type="image/x-icon" />
-				<link href="./style.css" as="style" rel="stylesheet" type="text/css" />
+				<link href="./images/favicon.ico?v=1" rel="shortcut icon" type="image/x-icon" />
+				<link href="./style/global.css" as="style" rel="stylesheet" type="text/css" />
 				<link href="https://cdn.jsdelivr.net/npm/daisyui@3.8.2/dist/full.css" rel="stylesheet" type="text/css" />
 				<link href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.36.0/tabler-icons.min.css" rel="stylesheet" type="text/css" />
 				<link href="https://unpkg.com/@highlightjs/cdn-assets@11.8.0/styles/tokyo-night-dark.min.css" rel="stylesheet" type="text/css" />
 			</head>
-			<body>
+			<body class="subpixel-antialiased">
 				<Component />
 			</body>
 		</html>
