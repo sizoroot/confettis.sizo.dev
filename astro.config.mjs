44a36e4dd850aeabf0505990917e92cc355bd7f9
Lucas O. S. <ovniroto@gmail.com>
2025-07-03 11:48:07 +0200

    chore: add node to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,27 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
+  site: 'https://confettis.sizo.dev',
+  output: 'server',
+
+  build: {
+      inlineStylesheets: 'always'
+  },
+
+  integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  adapter: node({
+    mode: 'standalone',
+  }),
+  
 });
